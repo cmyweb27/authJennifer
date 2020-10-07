863a77908a05705419b844076eeae1d0519d0bf4
cmyweb <cmyweb@yahoo.com>
2020-10-07 20:10:05 +0100

    react router added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Navbar from "./components/Navbar";
 import MenuCard from "./components/MenuCard";
 import Home from "./components/homeComponents/Home";
 import Footer from "./components/Footer";
-
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <body>
         <div id="main-wrapper">
           <Navbar />
-          {/* <MenuCard /> */}
-          <Home />
+          <Switch>
+            <Route component={Home} exact path="/" />
+            <Route component={MenuCard} path="/menu" />
+          </Switch>
+
           <Footer />
         </div>
       </body>
